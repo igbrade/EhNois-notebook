d4af5e5b03f9be7535e8a0a761650fab0fad77f8
rianradeck <60761801+rianradeck@users.noreply.github.com>
2021-08-20 15:51:43 -0300

    Update mtrx_mult.cpp

--- mtrx_mult.cpp
+++ mtrx_mult.cpp
@@ -1,66 +1,68 @@
+const int MOD = 1e9 + 7;
+
 template<size_t sz>
 struct mtrx
 {
-	union
-	{
-		int data[sz * sz];
-		int elements[sz][sz];
-	};
+    union
+    {
+        int data[sz * sz];
+        int elements[sz][sz];
+    };
 
-	mtrx(int diagonal) : data{0}
-	{
-		for(int i = 0; i < sz; ++i)
-			elements[i][i] = diagonal;
-	}
+    mtrx(int diagonal) : data{0}
+    {
+        for(int i = 0; i < sz; ++i)
+            elements[i][i] = diagonal;
+    }
 
-	template<typename... Args>
-	mtrx(Args&&... args) : data{args...}
-	{
-	}
+    template<typename... Args>
+    mtrx(Args&&... args) : data{args...}
+    {
+    }
 
-	mtrx<sz> operator* (const mtrx<sz> &o) const
-	{
-		mtrx<sz> ret;
-		for(int i = 0; i < sz; ++i)
-			for(int j = 0; j < sz; ++j)
-			{
-				for(int k = 0; k < sz; ++k)
-					ret.elements[i][j] += elements[i][k] * o.elements[k][j];
-			}
-		return ret;
-	}
+    mtrx<sz> operator* (const mtrx<sz> &o) const
+    {
+        mtrx<sz> ret;
+        for(int i = 0; i < sz; ++i)
+            for(int j = 0; j < sz; ++j)
+            {
+                for(int k = 0; k < sz; ++k)
+                    ret.elements[i][j] = (0ll + ret.elements[i][j] + (1ll * elements[i][k] * o.elements[k][j]) % MOD) % MOD;
+            }
+        return ret;
+    }
 
-	mtrx<sz> &operator*=(const mtrx<sz> &o)
-	{
-		(*this) = (*this) * o;
-		return (*this);
-	}
+    mtrx<sz> &operator*=(const mtrx<sz> &o)
+    {
+        (*this) = (*this) * o;
+        return (*this);
+    }
 };
 
 template<size_t sz>
 string to_string(const mtrx<sz> &m)
 {
-	stringstream s;
-	s << "{\n";
-	for(int i = 0; i < sz; ++i)
-	{
-		for(int j = 0; j < sz; ++j)
-			s << m.elements[i][j] << " ";
-		s << "\n";
-	}
-	s << "}\n";
-	return s.str();
+    stringstream s;
+    s << "{\n";
+    for(int i = 0; i < sz; ++i)
+    {
+        for(int j = 0; j < sz; ++j)
+            s << m.elements[i][j] << " ";
+        s << "\n";
+    }
+    s << "}\n";
+    return s.str();
 }
 
 template<typename T>
-T powm(const T &b, int e)
+T powm(const T &b, long long e)
 {
-	T ret = 1;
-	for(int i = __lg(e); i >= 0; --i)
-	{
-		ret = ret * ret;
-		if(e & (1 << i))
-			ret = b * ret;
-	}
-	return ret;
+    T ret = 1;
+    for(int i = __lg(e); i >= 0; --i)
+    {
+        ret = ret * ret;
+        if(e & (1ll << i))
+            ret = b * ret;
+    }
+    return ret;
 }
